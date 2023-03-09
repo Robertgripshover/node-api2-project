d18ddf8025498f2aa15f5acf2443ba60d6911976
Robert Gripshover <robertgrips@gmail.com>
2023-03-09 11:26:31 -0500

    almost finished with the get comments portion

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -129,8 +129,28 @@ router.put('/:id', (req, res) => {
     
 })
 
-router.get('/:id/messages', (req, res) => {
-    
+router.get('/:id/messages', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        //<<< setting a variable named 'post' as the id that has been
+        // selected in the '/:id' part, the 'Post' is being imported in at the top and is using dot notation to access
+        // the posts-model.js functions
+        if(!post) { //<< if the post does not exist this is what happens
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+
+        }        
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    }
 })
 
 
